cb5cb84ace9dabb6dcb892d1f04478a4c2cd30f9
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-12 14:00:30 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// 1-genero dei numeri casuali (passandoli in un array)
+
+// 2-visualizzo i seguenti numeri su schermo per un tot di secondi e assegno display none ; 
+
+// 3-countdown 30 secondi 
+
+// 4-inserisco i numeri visualizzati tramite prompt li confronto con quelli generati e restituisco quanti numeri ho indovinato
