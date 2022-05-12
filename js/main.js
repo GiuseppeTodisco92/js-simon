cb4869a77657ddfc0d05e5bfdaff78c7b18eb23c
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-12 15:16:48 +0100

    add number + countdown

--- js/main.js
+++ js/main.js
@@ -3,9 +3,77 @@
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-// 1-genero dei numeri casuali (passandoli in un array)
+/*--------------
+    FUNZIONI
+----------------*/
+// GENERA NUMERI RANDOMICI 
+function randomInteger(min,max){
+    return Math.floor(Math.random()* (max - min + 1)) +1 ;
+}
 
+function countDown(){
+    let y = 4
+    const container = document.querySelector(".container");
+    const countNumber = document.createElement("div");
+    let countdown = setInterval(function(){
+            console.log(y);
+            countNumber.innerHTML= y;
+            container.append(countNumber);
+            
+            if (y == 0){
+                clearInterval(countdown);
+            } 
+            y--;
+            
+    },1000)
+}
+
+/*--------------
+    MAIN
+----------------*/
+let numbers = [];
+let i = 0 ;
+const n = 5;
+
+const btn = document.querySelector(".btn_custom");
+const btnBox = document.querySelector(".box_btn")
+let numberList = document.querySelector(".numberlist");
+    numberList.classList.add("display_none")
+
+// 1-genero dei numeri casuali (passandoli in un array)
+while( numbers.length < n){
+    let number = randomInteger(1,20);
+    if ( numbers.includes(number) === false ){
+        numbers.push(number);
+    } 
+}
+console.log(numbers)
 // 2-visualizzo i seguenti numeri su schermo per un tot di secondi e assegno display none ; 
+btn.addEventListener("click",function(){
+    btn.classList.add("display_none");
+    btnBox.classList.add("display_none");
+    numberList.classList.remove("display_none");
+    for (i=0; i<numbers.length ;i++){
+        const boxNumber = document.createElement("div");
+        boxNumber.classList.add("box_style");
+        boxNumber.innerHTML = numbers[i];
+        numberList.append(boxNumber);
+    }
+    i = 0;
+    const timerNumber = setInterval(function(){
+        console.log(i);
+        i++;
+        if(i == 2){
+            clearInterval(timerNumber);
+            numberList.classList.add("display_none");
+            countDown();
+        }
+},1000)
+
+})
+
+
+
 
 // 3-countdown 30 secondi 
 
